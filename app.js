700a100045e7494cec97b04fc16768c057e3ca4c
Mathias Hidalgo Caja <91381083+MathiasHidalgo@users.noreply.github.com>
2022-03-07 22:55:04 -0500

    first

---
+++ app.js
@@ -0,0 +1,52 @@
+
+(function(){
+
+const openCloseButton = document.querySelector('#buttonOpenCloseMenu');
+const menuMobileItems = document.querySelector('#menu_mobile_items');
+const buttonNext = document.querySelector('#next');
+const buttonBefore = document.querySelector('#before');
+const sliders = document.querySelectorAll('.comments_body');
+let value;
+
+
+
+//MENU RESPONSIVE, BOTON
+openCloseButton.addEventListener('click', e =>{
+    menuMobileItems.classList.toggle('menu_mobile_closed');
+});
+
+//BUTTOMS COMMENTS
+buttonNext.addEventListener('click', () =>{
+    changePosition(1);
+})
+buttonBefore.addEventListener('click', () =>{
+    changePosition(-1);
+})
+
+//COMENTS SLIDERS
+const changePosition = (add) =>{
+    const currentComment = document.querySelector('.comments_body--show').dataset.id;
+    value = Number(currentComment);
+    value += add;
+
+    sliders[Number(currentComment)-1].classList.remove('comments_body--show');
+    if(value === sliders.length+1 || value == 0){
+        value = value === 0 ? sliders.length : 1;
+    }
+    
+    sliders[value-1].classList.add('comments_body--show');
+}
+
+//SCROLLNAV
+window.addEventListener('scroll', function(){
+    var menuScroll = document.querySelector("header");
+    menuScroll.classList.toggle("down", window.scrollY > 0);
+});
+
+//SCROLLNAV RESPONSIVE
+window.addEventListener('scroll', function(){
+    var menuMobile = document.querySelector(".menu_mobile");
+    menuMobile.classList.toggle("down_mobile", window.scrollY > 0);
+});
+})();
+
