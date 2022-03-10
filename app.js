11a4778f3982e6d9841d63d3c4221627d85aee62
MathiasHidalgo <mejhc2307lbc@gmail.com>
2022-03-10 12:47:35 -0500

    Terminado v1
    Falta llenar los datos de la pagina en general

--- app.js
+++ app.js
@@ -50,3 +50,23 @@ window.addEventListener('scroll', function(){
 });
 })();
 
+
+const $form = document.querySelector('#form');
+const $buttonMailto = document.querySelector('#mailto')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault();
+    const form = new FormData(this);
+    const  response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if(response.ok){
+        alert('Gracias por contactarme, en unos momentos contesto')
+    }
+}
